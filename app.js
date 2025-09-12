4796865206ce64ade57e62399c08a2a8776a4f3a
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-12 14:28:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const editPlayer1BtnEl = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnEl = document.getElementById('edit-player-2-btn');
+
+function openPlayerConfig() {}
+
+editPlayer1BtnEl.addEventListener('click', openPlayerConfig);
