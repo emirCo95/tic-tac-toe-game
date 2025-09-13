7aaee2c6cbbe713114085cbf8a588459d4ced11a
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-13 07:48:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const playerConfigOverlayEl = document.getElementById('config-overlay');
 const backdropEl = document.getElementById('backdrop');
 
+const formElement = document.querySelector('form');
+
 const editPlayer1BtnEl = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnEl = document.getElementById('edit-player-2-btn');
 
@@ -16,8 +18,12 @@ function closePlayerConfig() {
   backdropEl.style.display = 'none';
 }
 
+function savePlayerConfig() {}
+
 editPlayer1BtnEl.addEventListener('click', openPlayerConfig);
 editPlayer2BtnEl.addEventListener('click', openPlayerConfig);
 
 cancelConfigBtnEl.addEventListener('click', closePlayerConfig);
 backdropEl.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig);
