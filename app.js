008f1d0d9e750f05cd92b0cb970c89f891198b86
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-16 12:46:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -73,9 +73,18 @@ function startNewGame() {
   gameAreaEl.style.display = 'block';
 }
 
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+}
+
 function selectGameField(event) {
   event.target.textContent = players[activePlayer].symbol;
   event.target.classList.add('disabled');
+  switchPlayer();
 }
 
 editPlayer1BtnEl.addEventListener('click', openPlayerConfig);
